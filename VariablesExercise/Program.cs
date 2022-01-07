99b1a7c619925a7db91a04f7e46a9a964dec86da
Jaylon Carter <carterjay2001@outlook.com>
2022-01-07 10:18:25 -0600

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Jaylon";
+            int myAge = 20;
+            char myInitial = 'J';
+            bool loveLife = true;
+            double myHairs = 115.3;
+            decimal myWeight = 155.7m;
+
+            Console.WriteLine($"My name is {myName}, I am {myAge} years old and " +
+                $"weigh {myWeight} pounds! Sometimes I go by {myInitial} and if " +
+                $"you're wondering if I love life it's {loveLife}. Fun fact, I " +
+                $"have {myHairs} thousand hairs on my head!");
+        }
+    }
+}
